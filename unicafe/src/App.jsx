f518511e09204ad33da735e11fbce41f5a8947b6
Carmen Jara <jarac3727@gmail.com>
2026-03-29 19:14:21 -0600

    Ejercicio 1.10

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+
+const StatisticLine = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  )
+}
+
 const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad
+
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
   const average = (props.good - props.bad) / total
   const positive = (props.good / total) * 100
 
   return (
     <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>total {total}</p>
-      <p>average {total === 0 ? 0 : average}</p>
-      <p>positive {total === 0 ? 0 : positive} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + ' %'} />
     </div>
   )
 }
@@ -21,16 +38,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-if (total === 0) {
-  return <p>No feedback given</p>
-}
+
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
